41580f5abc0b167dcc67c53e531253562089699c
fernandoarrj <fernandoarrj@gmail.com>
2018-09-16 15:02:02 -0300

    add model task and test if save object just with name task

--- backend/tasks/app/models.py
+++ backend/tasks/app/models.py
@@ -1,1 +1,14 @@
 from django.db import models
+
+class Task(models.Model):
+	name = models.CharField('Tarefa', max_length=200)
+	done = models.BooleanField('Feito', blank=True, default=False)
+	dtcreate = models.DateField('Data de Criação', auto_now_add=True)
+	dtdone = models.DateField('Data de Finalização', blank=True, null=True)
+	
+	class Meta:
+		verbose_name = 'Tarefa'
+		verbose_name_plural = 'Tarefas'
+		
+	def __str__(self):
+		return self.name
--- backend/tasks/app/tests.py
+++ backend/tasks/app/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from django.core.exceptions import ValidationError
+from app.models import Task
 
 # Create your tests here.
+
+class TaskModelTest(TestCase):
+	
+	def test_create_task_just_with_name(self):
+		task = Task.objects.create(name='Move chairs from other room.')
+		self.assertEqual(Task.objects.count(), 1)
