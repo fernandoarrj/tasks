91f96f1b0d99c1bef7a7163ac50ad71e67a2fcfa
fernandoarrj <fernandoarrj@gmail.com>
2018-09-16 13:01:22 -0300

    Adding rest_framework, create app for api and migrate

--- backend/tasks/tasks/settings.py
+++ backend/tasks/tasks/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # libs install by me
+    'rest_framework',
+    
+    # apps
+    'app',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +124,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Rest Framework settings
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
