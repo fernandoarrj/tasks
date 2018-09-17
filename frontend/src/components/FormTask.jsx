18460b8e86575807c067c6aa0c743be978e539be
fernandoarrj <fernandoarrj@gmail.com>
2018-09-16 22:41:43 -0300

    create formtask for post and viewtasks

--- frontend/src/components/FormTask.jsx
+++ frontend/src/components/FormTask.jsx
@@ -1,30 +1,151 @@
 import React from 'react'
+import axios from 'axios'
+import ReactDOM from 'react-dom'
 
 import './FormTask.css'
 
+const baseUrl = 'http://127.0.0.1:8000/api/'
+
+class ErrorService extends React.Component {
+	render () {
+		return (
+			<div>
+				<p className="alert alert-danger">
+					Erro com o servidor, por favor teste novamente.
+				</p>
+			</div>
+		)
+	}
+}
+
+class ErrorMessage extends React.Component {
+	
+	constructor(props) {
+		super(props)
+		this.errMsg = this.props.errMsg
+	}
+	
+	render () {
+		return (
+			<div>
+				<p className="alert alert-danger">{this.errMsg}</p>
+			</div>
+		)
+	}
+}
+
+class SuccessMessage extends React.Component {
+	render() {
+		return (
+			<div>
+				<p className="alert alert-success">Task has been created.</p>
+			</div>
+		)
+	}
+}
+
 class FormTask extends React.Component {
+	
+	constructor(props) {
+		super(props)
+		this.handleFormSubmit = this.handleFormSubmit.bind(this)
+		this.clearErrorMessage = this.clearErrorMessage.bind(this)
+
+		this.errMsg = ''
+	}
+	
+	validationForm(name, done, dtdone) {
+		if (name === '') {
+			this.errMsg = 'You have to describe the task.'
+			return false
+		}
+		return true
+	}
+	
+	hasError() {
+		ReactDOM.render(
+			<ErrorMessage errMsg={this.errMsg}/>, 
+			document.getElementById('errorMessage')
+		)
+	}
+	
+	clearErrorMessage() {
+		ReactDOM.render(
+			<p></p>, 
+			document.getElementById('errorMessage')
+		)
+	}
+	
+	sucessMessage() {
+		ReactDOM.render(
+			<SuccessMessage />, 
+			document.getElementById('errorMessage')
+		)
+		setTimeout(() => {
+			ReactDOM.render(
+				<p></p>, 
+				document.getElementById('errorMessage')
+			)
+		}, 10 * 1000)
+	}
+	
+	errorService() {
+		ReactDOM.render(
+			<ErrorService />,
+			document.getElementById('errorMessage')
+		)
+	}
+	
+	handleFormSubmit(e) {
+		e.preventDefault()
+		const name = e.target.elements.nameTask.value
+		const done = e.target.elements.doneTask.checked
+		let dtdone = e.target.elements.dtdoneTask.value
+		
+		if (dtdone === '') { dtdone = null }
+		
+		
+		if (this.validationForm(name, done, dtdone)) {
+			axios.post(baseUrl, {
+				name,
+				done,
+				dtdone
+			})
+			.then(this.sucessMessage)
+			.catch(this.errorService)
+			
+		} else {
+			return this.hasError()
+		}
+		
+	}
+	
 	render() {
 		return (
 			<div>
 				<h1> Create Task </h1>
 				
-				<form>
+				<div id="errorMessage">
+				
+				</div>
+				
+				<form onSubmit={this.handleFormSubmit} method="POST">
 					<div className="form-group">
 						<label htmlFor="nameTask">Task</label>
 						<input type="text" className="form-control" id="nameTask" 
-							name="nameTask" placeholder="Task"/> 
+							name="nameTask" placeholder="Task" onChange={this.clearErrorMessage}/> 
 					</div>
 					
 					<div className="form-group form-check">
 						<input type="checkbox" className="form-check-input" id="doneTask" 
-							name="doneTask" />
+							name="doneTask"/>
 						<label className="form-check-label" htmlFor="doneTask">Done</label>
 					</div>
 					
 					<div className="form-group">
 						<label htmlFor="nameTask">Date finish</label>
-						<input type="date" className="form-control" id="task" 
-							name="task" placeholder="Task"/> 
+						<input type="date" className="form-control" id="dtdoneTask" 
+							name="dtdoneTask" placeholder="Done"/> 
 					</div>
 					
 					<button className="btn btn-success">Create Task</button>
@@ -33,5 +154,6 @@ class FormTask extends React.Component {
 		)
 	}
 }
+
 
 export default FormTask
--- frontend/src/components/ViewTask.jsx
+++ frontend/src/components/ViewTask.jsx
@@ -3,14 +3,22 @@ import axios from 'axios'
 
 import './ViewTask.css'
 
+const baseUrl = 'http://127.0.0.1:8000/api/'
+
 class ViewTask extends React.Component {
 	
 	state = {
 		tasks : []
 	}
 	
+	constructor(props) {
+		super(props)
+		
+		this.deleteRegister = this.deleteRegister.bind(this)
+	}
+	
 	componentDidMount() {
-		axios.get('http://127.0.0.1:8000/api/')
+		axios.get(baseUrl)
 			.then(res => {
 				this.setState({
 					tasks: res.data
@@ -20,18 +28,31 @@ class ViewTask extends React.Component {
 			
 	}
 	
+	deleteRegister() {
+		const idTask = 1
+		axios.delete(`${baseUrl}${idTask}`)
+			.then(res => console.log(res))
+			.catch(err => console.log(err))
+	}
+	
+	renderTasks() {
+		return this.state.tasks.map(function(e) {
+			return (
+				<div key={e.id} className="view-task">
+					<p>Task - {e.name}</p>
+					<p>{e.done ? 'Finished' : "don't finished" }</p>
+					<p>{e.dtdone ? e.dtdone : null}</p>
+					<button className="btn btn-danger">Delete Task</button>
+				</div>
+				
+			)
+		})
+	}
+	
 	render() {
 		return (
 			<React.Fragment>
-				{this.state.tasks.map(function(e) {
-					return (
-						<div key={e.id} className="view-task">
-							<p>{e.name}</p>
-							<p>{e.dtcreate}</p>
-							<p>{e.dtdone}</p>
-						</div>
-					)
-				})}
+				{this.renderTasks()}
 			</React.Fragment>
 		)
 	}
